2bd58d7b179b710dc3045da4f1a277406ec67430
ruhany-aragao <ruhany.aragao@gmail.com>
2023-02-28 15:14:04 -0300

    + Plotly8

--- js/script.js
+++ js/script.js
@@ -32,7 +32,8 @@ async function getDataFromBacen(strSerie="20749") {
           const x = [];
           const y = [];
           for (const i of dataSet) {
-            x.push(new Date(i.data));
+
+            x.push(new Date(new Date(i.data).toLocaleDateString('en-GB')));
             y.push(Number(i.valor))
         }
           const data = {x,y}
@@ -43,7 +44,6 @@ async function getDataFromBacen(strSerie="20749") {
         }
       
     }
-
 
 async function plotSeries(seriesName) {
     const data = await getDataFromBacen(mapSerieToName[seriesName]);
@@ -61,39 +61,77 @@ async function plotSeries(seriesName) {
         }
     ], layout);
 }
-    //plotSeries('Saldos das operações de crédito das instituições financeiras sob controle privado nacional - Total');
 
-async function plotMultipleSeries(seriesNames) {
-    
-    const arrayBacenData = {}
-    
-    //get promise from API
-    for(const i=0; i<seriesNames.length; i++){
-      let arrayData = {};
-      arrayData = await getDataFromBacen(mapSerieToName[seriesNames[i]]);
-      arrayBacenData[seriesNames[i]] = arrayData;
-    }
-    //create series
-    const series = []
-    for(const i=0; i<seriesNames.length; i++){
-        series.push({
-            x: arrayBacenData[seriesNames[i]].x,
-            y: arrayBacenData[seriesNames[i]].y,
-            name: seriesNames[i]})
-    }
-    //create layout
+async function plotMultipleSeries(idDiv,seriesNames) {
+    //add a drop down to append multiple series
+
+    const data = await Promise.all(seriesNames.map(async (seriesName) => {
+        const data = await getDataFromBacen(mapSerieToName[seriesName]);
+        const x = data.x;
+        const y = data.y;
+        return {x,y,name:seriesName}
+    }));
+
     const layout = {
-        title: 'Série Temporal',
-        xaxis: {title: 'Data'},
-        yaxis: {title: 'Valor'}
+      //add second y axis option
+      yaxis2: {title: 'Valor',overlaying: 'y',side: 'right'},
+      
+      xaxis: {type: 'date',automargin: true,tickformat: '%m/%Y'},
+      legend: { orientation: "h", yanchor: "bottom", y: 1.02, xanchor: "right", x: 1 }
     };
-
-    //Plot
-    Plotly.newPlot('tester', series, layout);
+    console.log(data);
+    Plotly.newPlot(idDiv, data, layout);
 }
 
+//add options from mapSerieToName to drop down
+for (const serie in mapSerieToName) {
+  const option = document.createElement('option');
+  option.value = serie;
+  option.innerHTML = serie;
+  document.querySelector('#options-series').appendChild(option);
+}
+
+//plot multiple series from a option drop down selection using the plotMultipleSeries function
+const submitButton = document.querySelector('#button-submit');
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  //create button to add more series
+  const seriesName = document.querySelector('#options-series').value;
+  //add element with series name and closing button
+  const div = document.createElement('div');
+  div.innerHTML = `<span id = serie-${mapSerieToName[seriesName]}>${seriesName}</span><button id="close-btn-${mapSerieToName[seriesName]}">X</button>`;
+  console.log(div);
+  document.querySelector('.grafico').appendChild(div);
+});
+
+//delete series from div if close button is clicked
+document.querySelector('.grafico').addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const id = event.target.id;
+    const serieId = id.split('-')[2];
+    const serie = document.querySelector(`#serie-${serieId}`);
+    serie.parentNode.removeChild(serie);
+    event.target.parentNode.removeChild(event.target);
+  }});
+
+  //plot all series from div whenever add button or close button is clicked
+  document.querySelector('.grafico').addEventListener('click', (event) => {
+    const seriesNames = [];
+    const series = document.querySelectorAll('.grafico span');
+    for (const serie of series) {
+      seriesNames.push(serie.innerHTML);
+    };
+    plotMultipleSeries('grafico-1',seriesNames);
+  });
 
 
 
-    
-  plotMultipleSeries(['Saldos das operações de crédito das instituições financeiras sob controle privado nacional - Total','Taxa Preferencial Brasileira']);
+  
+  
+
+
+
+
+
+//plotMultipleSeries('grafico-1',['Inadimplência da carteira de crédito das instituições financeiras sob controle público - Total', 'Inadimplência da carteira de crédito das instituições financeiras sob controle privado nacional - Total', 'Inadimplência da carteira de crédito das instituições financeiras sob controle estrangeiro - Total']);
+
